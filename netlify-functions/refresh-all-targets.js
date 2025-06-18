02b27829fcb2af98f8d2b40cb234d587f18a02ce
AMBiSCA <billy.fergusonx@gmail.com>
2025-06-18 16:53:12 +0100

    update

--- netlify-functions/refresh-all-targets.js
+++ netlify-functions/refresh-all-targets.js
@@ -1,24 +1,8 @@
 exports.handler = async function(event, context) {
-    console.log("--- BARE BONES DEBUG FUNCTION STARTED ---");
-
-    // Check for the existence of the environment variables
-    const apiKey = process.env.ADMIN_TORN_API_KEY;
-    const creds = process.env.FIREBASE_CREDENTIALS_BASE64;
-
-    console.log("Found ADMIN_TORN_API_KEY:", !!apiKey); // This will log true or false
-    console.log("Found FIREBASE_CREDENTIALS_BASE64:", !!creds); // This will log true or false
-
-    if (creds) {
-        console.log("First 50 characters of credentials variable:", creds.substring(0, 50));
-    } else {
-        console.log("The FIREBASE_CREDENTIALS_BASE64 variable was not found by the function.");
-    }
-
-    console.log("--- BARE BONES DEBUG FUNCTION FINISHED ---");
-
-    // We are returning a custom error message on purpose so we know this version ran.
+    console.log("Hello World from the simplest possible function!");
+    
     return {
-        statusCode: 501, // Using 501 "Not Implemented" as a signal
-        body: "This was the bare-bones debug function. Please check the Netlify logs for the real output."
+        statusCode: 200,
+        body: "Hello World! The function ran successfully."
     };
 };
